a0a49b65e13b3ecac5a3a18d763a0d164c43725b
AntonStrakhov <gordnet_ctpax@mail.ru>
2024-07-22 08:38:56 +0300

    added befor after test annotations 2

--- src/test/java/test/Day1.java
+++ src/test/java/test/Day1.java
@@ -1,6 +1,8 @@
 package test;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Day1 {
@@ -14,6 +16,11 @@ public class Day1 {
     public void Demo() {
 
         System.out.println("hello");
+    }
+
+    @AfterSuite
+    public void afSuite() {
+        System.out.println("I am the no 1 from last");
     }
 
     @Test
--- src/test/java/test/Day3.java
+++ src/test/java/test/Day3.java
@@ -1,5 +1,8 @@
 package test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Day3 {
@@ -9,9 +12,24 @@ public class Day3 {
         System.out.println("weblogincar");
     }
 
+    @BeforeMethod
+    public void beforeEvery() {
+        System.out.println("I will execute before every method in day 3 class");
+    }
+
+    @AfterMethod
+    public void afterEvery() {
+        System.out.println("I will execute after every method in day 3 class");
+    }
+
     @Test
     public void MobileLoginCarLoan() {
         System.out.println("Mobilelogincar");
+    }
+
+    @BeforeSuite
+    public void bfSuite() {
+        System.out.println("I am no 1");
     }
 
     @Test
